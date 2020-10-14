5c80f99d00b832933cce2f23d07db38a911a9532
Ezequiel Gnich <ezequielgnich@live.com>
2020-10-14 11:04:10 -0300

    Criação das routes e reestruturação do código

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,13 @@
 import './styles/global.css'
-import './styles/pages/landing.css'
 
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi'
 
-import logoImg from './images/Logo.svg'
+import Routes from './routes'
+
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Logo Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Joinville</strong>
-          <span>Santa Catarina</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0,0,0,0.2)" />
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 

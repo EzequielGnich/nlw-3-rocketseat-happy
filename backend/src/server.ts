bfaccb5f8ed1d27645993182bb75fd733db3c280
Ezequiel Gnich <ezequielgnich@live.com>
2020-10-15 12:26:42 -0300

    reestruturação do código

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express'
+import path from 'path'
 
 import './database/connection'
+import routes from './routes'
 
 const app = express()
-app.use(express.json())
 
 const port = process.env.PORT || 3333
 
-app.get('/', (req, res) => {
-
-})
+app.use(express.json())
+app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(port, () => console.log(`Runing on port ${port}`))

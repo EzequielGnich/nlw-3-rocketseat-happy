c1052151a126d5303f1905ff75f17aa149461699
Ezequiel Gnich <ezequielgnich@live.com>
2020-10-17 20:02:13 -0300

    Pagina landing

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,14 +1,14 @@
-import '../styles/pages/landing.css'
+import "../styles/pages/landing.css";
 
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FiArrowRight } from 'react-icons/fi'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowRight } from "react-icons/fi";
 
-import logoImg from '../images/Logo.svg'
+import logoImg from "../images/Logo.svg";
 
 function Landing() {
-    return (
-        <div id="page-landing">
+  return (
+    <div id="page-landing">
       <div className="content-wrapper">
         <img src={logoImg} alt="Logo Happy" />
 
@@ -27,7 +27,7 @@ function Landing() {
         </Link>
       </div>
     </div>
-    )
+  );
 }
 
 export default Landing;

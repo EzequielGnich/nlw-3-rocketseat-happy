116ff6917fbabef6da20db357d862aedd4db2bed
Ezequiel Gnich <ezequielgnich@live.com>
2020-10-17 19:58:52 -0300

    server.ts backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,20 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import "express-async-errors";
+import path from "path";
+import cors from "cors";
 
-import './database/connection'
-import routes from './routes'
+import "./database/connection";
+import routes from "./routes";
+import errorHandler from "./errors/handler";
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3333;
 
-app.use(express.json())
-app.use(routes)
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Runing on port ${port}`))
+app.listen(port, () => console.log(`Runing on port ${port}`));

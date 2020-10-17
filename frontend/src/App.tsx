6233c85745f28ca136177037db5e7b98ccc8f3e1
Ezequiel Gnich <ezequielgnich@live.com>
2020-10-17 20:01:05 -0300

    Alteração da estrutura

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-import './styles/global.css'
+import "./styles/global.css";
+import "leaflet/dist/leaflet.css";
 
-import React from 'react';
+import React from "react";
 
-import Routes from './routes'
-
+import Routes from "./routes";
 
 function App() {
-  return (
-    <Routes />
-  );
+  return <Routes />;
 }
 
 export default App;
